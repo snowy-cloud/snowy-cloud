e54403d9774b4c40de8af7e660255e8e96fc2f75
program <147569510+snowy-cloud@users.noreply.github.com>
2023-11-22 15:27:57 +0530

    Update #1 sum.c

--- sum.c
+++ sum.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	int sum,a=2,b=3;
+	//int sum,a=2,b=3;
+	int sum,a,b;
+	printf("enter two no.s to be added together");
+	scanf("%d %d",&a,&b);
 	sum=a+b;
 	printf("the sum is : %d ",sum);
 	return 0;
